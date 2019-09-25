5d7bbeac75fd67240fc85a4dce503a5911988e4b
Gallagher94 <ruairidhgallaghers3@gmail.com>
2019-09-25 23:29:29 +0100

    ready for new deployment

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_CONNECTION_URL, {
+const MONGO_URL =
+  "mongodb+srv://ruairidhgallagher:Bb00230762@cluster0-y2nks.mongodb.net/winch-driving-app?retryWrites=true&w=majority";
+
+mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
