5c7bdfe3ff88972f0b7dbecb07e0bfed103d5491
Gallagher94 <ruairidhgallaghers3@gmail.com>
2019-09-26 00:05:38 +0100

    port change

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 
 require("./db/mongoose"); // file runs - mongoose connects to db
 
@@ -7,7 +7,7 @@ const userRouter = require("./routers/user");
 const app = express();
 
 app.use(express.json()); // parse incoming data to be json.
-app.use(cors());
+// app.use(cors());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
